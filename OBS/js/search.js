63de71d0f34b055a52baf4248c80ff3076837045
Arbin Niroula <137669272+arbeen-05@users.noreply.github.com>
2025-08-18 13:45:07 +0545

    Delete OBS directory

--- OBS/js/search.js
+++
@@ -1,7 +0,0 @@
-function searchTransaction(){
-  let id=document.getElementById('searchId').value;
-  let txns=JSON.parse(localStorage.getItem('obs_transactions')||'[]');
-  let txn=txns.find(x=>x.id===id);
-  if(txn) document.getElementById('searchResult').innerHTML=txn.html;
-  else document.getElementById('searchResult').innerHTML='Not found';
-}
