80f5fe973ef506857efd28aba00486e77ce35a8b
Arbin Niroula <137669272+arbeen-05@users.noreply.github.com>
2025-08-30 10:04:14 +0000

    navbar edited , made mobile friendly

--- js/contact.js
+++ js/contact.js
@@ -37,3 +37,20 @@ document.getElementById('contact-form').addEventListener('submit', e => {
 
   e.target.reset();
 });
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector("nav ul");
+const navLinks = document.querySelectorAll("nav ul li a");
+
+// Toggle menu
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("open");
+});
+
+// Auto-close when a nav link is clicked
+navLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("open");
+  });
+});
